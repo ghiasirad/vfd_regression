b9b6bb91536d5f6a09d84bc7adf5f8cdd35c5d99
admin <ghiasirad.milad@gmail.com>
2022-01-10 02:30:18 -0500

    Saved recipe: VFDREGRESSION.compute_Fearures_joined_Nanostring_Smogned

--- recipes/compute_Fearures_joined_Nanostring_Smogned.py
+++ recipes/compute_Fearures_joined_Nanostring_Smogned.py
@@ -1,20 +1,3 @@
-# Read recipe inputs
-Features_joined_Nanostring = dataiku.Dataset("Features_joined_Nanostring")
-Features_joined_Nanostring_df = Features_joined_Nanostring.get_dataframe()
-
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-Fearures_joined_Nanostring_Smogned_df = Features_joined_Nanostring_df # For this sample code, simply copy input to output
-
-
-# Write recipe outputs
-Fearures_joined_Nanostring_Smogned = dataiku.Dataset("Fearures_joined_Nanostring_Smogned")
-Fearures_joined_Nanostring_Smogned.write_with_schema(Fearures_joined_Nanostring_Smogned_df)
-
-
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -35,7 +18,7 @@ features_smogn = smogn.smoter(
     pert = 0.2
 )
 
-Fearures_joined_Nanostring_Smogned_df = Features_joined_Nanostring_df # For this sample code, simply copy input to output
+Fearures_joined_Nanostring_Smogned_df = features_smogn # For this sample code, simply copy input to output
 
 
 # Write recipe outputs

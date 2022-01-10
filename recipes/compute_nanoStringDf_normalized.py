2e39d7fa2984e982b9cfcf9e63c949669caeb083
admin <ghiasirad.milad@gmail.com>
2022-01-10 09:19:36 -0500

    Saved recipe: VFDREGRESSION.compute_nanoStringDf_normalized

--- recipes/compute_nanoStringDf_normalized.py
+++ recipes/compute_nanoStringDf_normalized.py
@@ -24,7 +24,7 @@ nanoString_normalized_df.insert(0, 'StudyIDUniversal', first_column)
 
 
 # Write recipe outputs
-nanoString_normalized = dataiku.Dataset("nanoString_normalized")
-nanoString_normalized.write_with_schema(nanoString_normalized_df)
+nanoStringDf_normalized = dataiku.Dataset("nanoString_normalized")
+nanoStringDf_normalized.write_with_schema(nanoString_normalized_df)
 
 

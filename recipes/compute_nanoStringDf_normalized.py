39df5e5cf8f1d6914e729338286e856ecc8d8737
admin <ghiasirad.milad@gmail.com>
2022-01-10 09:14:26 -0500

    Saved recipe: VFDREGRESSION.compute_nanoStringDf_normalized

--- recipes/compute_nanoStringDf_normalized.py
+++ recipes/compute_nanoStringDf_normalized.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
-
 # Read recipe inputs
 nanoStringDf = dataiku.Dataset("nanoStringDf")
 nanoStringDf_df = nanoStringDf.get_dataframe()
@@ -18,3 +13,40 @@ nanoStringDf_normalized_df = nanoStringDf_df # For this sample code, simply copy
 # Write recipe outputs
 nanoStringDf_normalized = dataiku.Dataset("nanoStringDf_normalized")
 nanoStringDf_normalized.write_with_schema(nanoStringDf_normalized_df)
+
+
+
+
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+from sklearn.preprocessing import normalize
+
+# Read recipe inputs
+nanoStringDf = dataiku.Dataset("nanoStringDf")
+nanoStringDf_df = nanoStringDf.get_dataframe()
+
+
+# Normalize and return it as a dataframe
+amioAcids_cleaned_df = nanoStringDf_df.set_index('StudyID_Int')
+
+# Remove the empty cells and replace them with median of the column
+amioAcids_cleaned_df.fillna(amioAcids_cleaned_df.median(), inplace=True)
+amioAcids_normalized_values = normalize(amioAcids_cleaned_df.values, axis=1, norm='l1')
+
+amioAcids_normalized_df = pd.DataFrame(data=amioAcids_normalized_values, columns=amioAcids_cleaned_df.columns)
+amioAcids_normalized_df['StudyID_Int'] = amioAcids_cleaned_df.index
+
+first_column = amioAcids_normalized_df.pop('StudyID_Int')
+amioAcids_normalized_df.insert(0, 'StudyID_Int', first_column)
+
+
+aminoacids_normalized_df = amioAcids_normalized_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+aminoacids_normalized = dataiku.Dataset("Aminoacids_normalized")
+aminoacids_normalized.write_with_schema(aminoacids_normalized_df)
+
+

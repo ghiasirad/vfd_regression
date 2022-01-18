1b9208730eb9d945d4eb6c3119e81d826970d271
admin <ghiasirad.milad@gmail.com>
2022-01-17 23:35:41 -0500

    Saved recipe: VFDREGRESSION.compute_DemographicData_prepared_normal

--- recipes/compute_DemographicData_prepared_normal.py
+++ recipes/compute_DemographicData_prepared_normal.py
@@ -32,13 +32,13 @@ DemographicData_prepared_df = DemographicData_prepared.get_dataframe()
 
 
 # Normalize and return it as a dataframe
-nanoString_cleaned_df = DemographicData_prepared_df.set_index('StudyID')
+DemographicData_prepared_df = DemographicData_prepared_df.set_index('StudyID')
 
 # Remove the empty cells and replace them with median of the column
-nanoString_cleaned_df.fillna(nanoString_cleaned_df.median(), inplace=True)
+DemographicData_prepared_df.fillna(DemographicData_prepared_df.median(), inplace=True)
 # nanoString_normalized_values = normalize(nanoString_cleaned_df.values, axis=1, norm='l1')
 min_max_scaler = preprocessing.MinMaxScaler()
-nanoString_normalized_values = min_max_scaler.fit_transform(nanoString_cleaned_df.values)
+DemographicData_normalized_values = min_max_scaler.fit_transform(DemographicData_prepared_df.values)
 
 nanoString_normalized_df = pd.DataFrame(data=nanoString_normalized_values, columns=nanoString_cleaned_df.columns)
 nanoString_normalized_df['StudyIDUniversal'] = nanoString_cleaned_df.index

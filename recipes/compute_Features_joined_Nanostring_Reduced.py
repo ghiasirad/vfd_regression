5994f87ec046aaac5724e9c9b61af1f2523f456b
admin <ghiasirad.milad@gmail.com>
2022-01-18 00:33:22 -0500

    Saved recipe: VFDREGRESSION.compute_Features_joined_Nanostring_Reduced

---
+++ recipes/compute_Features_joined_Nanostring_Reduced.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+Features_joined_Nanostring = dataiku.Dataset("Features_joined_Nanostring")
+Features_joined_Nanostring_df = Features_joined_Nanostring.get_dataframe()
+
+
+import matplotlib.pyplot as plt
+
+def applyLimitOnRemainedFeatures(totalDf, cutoff=0):
+    
+    tgt = totalDf.pop('VFD')
+    fDf = totalDf
+    
+    # Create correlation matrix
+    corr_matrix = fDf.corr().abs()
+
+    # Select upper triangle of correlation matrix
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
+
+    # print(upper)
+
+    # Find index of feature columns with correlation greater than 0.95
+    to_drop = [column for column in upper.columns if any(upper[column] > 0.8)]
+
+#     print(to_drop)
+    # print(len(to_drop))
+
+    # Drop correlated features 
+    featuresDf_nonCorr = fDf.drop(to_drop, axis=1)
+#     featuresDf_nonCorr = fDf
+
+#     print(featuresDf_nonCorr)
+    print(featuresDf_nonCorr.shape)
+#     print(featuresDf_nonCorr.columns)
+
+    from sklearn.linear_model import ElasticNetCV
+
+#     ElasticNetModel = ElasticNetCV(cv=10, random_state=42)
+    ElasticNetModel = ElasticNetCV(random_state=42)
+    ElasticNetModel.fit(featuresDf_nonCorr, tgt)
+
+    # Assigns weight to each feature
+    coefs = ElasticNetModel.coef_
+#     print(coefs)
+    # print(len(coefs))
+#     print(fsDf.shape)
+
+    # Make a dictionary of the metabolites and their weights
+    remainedCols = {featuresDf_nonCorr.columns[i]:coefs[i] for i in range(featuresDf_nonCorr.shape[1]) if abs(coefs[i]) > cutoff}
+
+    # Sort the dictionary of non zero values
+    remainedColsSorted = sorted({x:y for x,y in remainedCols.items() if y!=0}.items(), key=lambda x: x[1])
+
+#     print(remainedCols)
+#     print(remainedColsSorted)
+
+
+    # extrac    # extract keys and vals to plot in a bar chart
+    # keys and vals to plot in a bar chart
+    keys = []
+    vals = []
+    geneKeys = []
+    geneVals = []
+    for i in range(1,len(remainedCols)+1):
+        keys.append(str(remainedColsSorted[-i][0]))
+        vals.append(remainedColsSorted[-i][1])
+
+#     print(len(keys))
+    print(keys)
+    print(vals)
+    
+    # Drop the low score columns
+    featuresDfReduced = fDf[keys]
+    print(featuresDfReduced.shape)
+    
+    plt.figure(figsize=(10,6))
+    plt.xticks(rotation=90)
+    plt.bar(keys, vals)
+    plt.ylabel('Normalized ElasticNet Importance Coefs')
+
+    plt.savefig('figs/topFeatures_' + str(cutoff) + '.pdf')
+    
+    featuresDfReducedSmoteTotal = featuresDfReduced
+    featuresDfReducedSmoteTotal['VFD'] = tgt
+    
+    return keys, featuresDfReducedSmoteTotal
+
+Features_joined_Nanostring_Reduced_df = Features_joined_Nanostring_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+Features_joined_Nanostring_Reduced = dataiku.Dataset("Features_joined_Nanostring_Reduced")
+Features_joined_Nanostring_Reduced.write_with_schema(Features_joined_Nanostring_Reduced_df)

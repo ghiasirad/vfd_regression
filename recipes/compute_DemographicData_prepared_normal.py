89b7a1b43a317d0ead59f5b906b08ea91e7277ba
admin <ghiasirad.milad@gmail.com>
2022-01-17 23:34:15 -0500

    Saved recipe: VFDREGRESSION.compute_DemographicData_prepared_normal

--- recipes/compute_DemographicData_prepared_normal.py
+++ recipes/compute_DemographicData_prepared_normal.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
+
 
 # Read recipe inputs
 DemographicData_prepared = dataiku.Dataset("DemographicData_prepared")
@@ -18,3 +15,41 @@ DemographicData_prepared_normal_df = DemographicData_prepared_df # For this samp
 # Write recipe outputs
 DemographicData_prepared_normal = dataiku.Dataset("DemographicData_prepared_normal")
 DemographicData_prepared_normal.write_with_schema(DemographicData_prepared_normal_df)
+
+
+
+
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+# from sklearn.preprocessing import normalize
+from sklearn import preprocessing
+
+# Read recipe inputs
+DemographicData_prepared = dataiku.Dataset("DemographicData_prepared")
+DemographicData_prepared_df = DemographicData_prepared.get_dataframe()
+
+
+# Normalize and return it as a dataframe
+nanoString_cleaned_df = DemographicData_prepared_df.set_index('StudyIDUniversal')
+
+# Remove the empty cells and replace them with median of the column
+nanoString_cleaned_df.fillna(nanoString_cleaned_df.median(), inplace=True)
+# nanoString_normalized_values = normalize(nanoString_cleaned_df.values, axis=1, norm='l1')
+min_max_scaler = preprocessing.MinMaxScaler()
+nanoString_normalized_values = min_max_scaler.fit_transform(nanoString_cleaned_df.values)
+
+nanoString_normalized_df = pd.DataFrame(data=nanoString_normalized_values, columns=nanoString_cleaned_df.columns)
+nanoString_normalized_df['StudyIDUniversal'] = nanoString_cleaned_df.index
+
+first_column = nanoString_normalized_df.pop('StudyIDUniversal')
+nanoString_normalized_df.insert(0, 'StudyIDUniversal', first_column)
+
+
+# Write recipe outputs
+nanoStringDf_normalized = dataiku.Dataset("nanoStringDf_normalized")
+nanoStringDf_normalized.write_with_schema(nanoString_normalized_df)
+
+
+

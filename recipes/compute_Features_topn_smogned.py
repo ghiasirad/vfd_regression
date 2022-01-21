c76c6134c9adb7655b9a86a044a6b5ace919b5a8
admin <ghiasirad.milad@gmail.com>
2022-01-21 10:09:59 -0500

    Saved recipe: VFDREGRESSION.compute_Features_topn_smogned

--- recipes/compute_Features_topn_smogned.py
+++ recipes/compute_Features_topn_smogned.py
@@ -25,25 +25,3 @@ Features_topn_smogned_df = features_smogn # For this sample code, simply copy in
 Features_topn_smogned = dataiku.Dataset("Features_topn_smogned")
 Features_topn_smogned.write_with_schema(Features_topn_smogned_df)
 
-
-# -*- coding: utf-8 -*-
-import dataiku
-import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
-import smogn
-
-# Read recipe inputs
-features_joined = dataiku.Dataset("Features_joined")
-features_joined_df = features_joined.get_dataframe()
-
-
-## conduct smogn
-features_smogn = smogn.smoter(
-    
-    data = features_joined_df, 
-    y = "VFD",
-    k = 3,
-    pert = 0.2
-)
-
-features_smogned_df = features_smogn # For this sample code, simply copy input to output

7aaef24a73010b242ef3139556e1c7e7c619513a
admin <ghiasirad.milad@gmail.com>
2022-01-21 10:06:32 -0500

    Saved recipe: VFDREGRESSION.compute_Features_joined_Nanostring_topn

--- recipes/compute_Features_joined_Nanostring_topn.py
+++ recipes/compute_Features_joined_Nanostring_topn.py
@@ -13,8 +13,10 @@ selectedFeatures_topn_df = selectedFeatures_topn.get_dataframe()
 # Filter the dataframe into a smaller dataset
 Features_joined_Nanostring_topn_df = Features_joined_Nanostring_df[selectedFeatures_topn_df.line.values]
 Features_joined_Nanostring_topn_df['StudyID_Int'] = Features_joined_Nanostring_df['StudyID_Int']
-Features_joined_Nanostring_topn_df.set_index('StudyID_Int')
 Features_joined_Nanostring_topn_df['VFD'] = Features_joined_Nanostring_df['VFD']
+
+first_column = nanoString_normalized_df.pop('StudyID_Int')
+nanoString_normalized_df.insert(0, 'StudyID_Int', first_column)
 
 
 # Write recipe outputs

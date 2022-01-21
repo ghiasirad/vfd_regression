25bcf0c5495b93e234e5eca1e908c238a5e47f87
admin <ghiasirad.milad@gmail.com>
2022-01-21 09:57:46 -0500

    Saved recipe: VFDREGRESSION.compute_Features_joined_Nanostring_topn

---
+++ recipes/compute_Features_joined_Nanostring_topn.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+Features_joined_Nanostring = dataiku.Dataset("Features_joined_Nanostring")
+Features_joined_Nanostring_df = Features_joined_Nanostring.get_dataframe()
+selectedFeatures_topn = dataiku.Dataset("selectedFeatures_topn")
+selectedFeatures_topn_df = selectedFeatures_topn.get_dataframe()
+
+
+# Filter the dataframe into a smaller dataset
+Features_joined_Nanostring_topn_df = Features_joined_Nanostring_df[selectedFeatures_topn_df.values]
+
+
+# Write recipe outputs
+Features_joined_Nanostring_topn = dataiku.Dataset("Features_joined_Nanostring_topn")
+Features_joined_Nanostring_topn.write_with_schema(Features_joined_Nanostring_topn_df)

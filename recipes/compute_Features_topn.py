52358afba40f8408fa93e11187184adb0f084b02
admin <ghiasirad.milad@gmail.com>
2022-01-23 20:36:21 -0500

    Saved recipe: VFDREGRESSION.compute_Features_topn

---
+++ recipes/compute_Features_topn.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+Features_joined_Nanostring = dataiku.Dataset("Features_joined_Nanostring")
+Features_joined_Nanostring_df = Features_joined_Nanostring.get_dataframe()
+selectedFeatures = dataiku.Dataset("selectedFeatures")
+selectedFeatures_df = selectedFeatures.get_dataframe()
+
+
+Features_topn_df = Features_joined_Nanostring_df[selectedFeatures_df.Feature.values[:30]]
+
+
+# Write recipe outputs
+Features_topn = dataiku.Dataset("Features_topn")
+Features_topn.write_with_schema(Features_topn_df)
